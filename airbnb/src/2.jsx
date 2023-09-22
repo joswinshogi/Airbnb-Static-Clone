881ed393fe801896fec449fcb8c7aa75edce4a00
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2023-09-22 19:09:52 +0530

    sixth commit

--- airbnb/src/2.jsx
+++ airbnb/src/2.jsx
@@ -1,7 +1,7 @@
 function Hero(){
     return(
         <section className="hero">
-            <img src="/Users/admin/Desktop/r1/Airbnb-Static-Clone/airbnb/photo-grid.png" className="hero--logo"></img>
+            <img src="/photo-grid.png" className="hero--logo"></img>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
